604b94e9a317af490d8ffccdafa3ddf53abab77d
SaiAvishkarSreerama <SaiAvishkar.Sreerama@gmail.com>
2019-09-29 17:12:49 -0500

    AviCommit: Leet Code preperation Easy modules with efficeint coding

---
+++ AviPreperation/LeetCodePrep/1.Easy/1.ArraySum.cs
@@ -0,0 +1,56 @@
+﻿//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//Example:
+//Given nums = [2, 7, 11, 15], target = 9,
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
+
+using System.Collections.Generic;
+namespace AviPreperation.LeetCodePrep._1.Easy
+{
+    public class TwoSumSol
+    {
+        /********My Solution********* --> O(n^2) *****/
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[] { };
+
+            //verfying each element with other element for only ones
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result = new[] { i, j };
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        /********Recommended Solution********* --> O(n) *****/
+        public int[] TwoSum1(int[] nums, int target)
+        {
+            //declare a dictionary sum
+            var sum = new Dictionary<int, int>();
+
+            //looping the given array
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //if the target - nums[i] whcih is our value is in Dictionary then returns that value
+                //for i=0 --> Here intitially dictionary is empty, target-nums[i] will not find then inserting the key:num[i] and value:index into the dictioanry
+                //for i=1 --> We already added nums[i] in it then if the target-nums[i] === num[0] that we inserted before then it treturns the nums[i]
+                if (sum.ContainsKey(target - nums[i]))
+                    return new int[] { sum[target - nums[i]], i };
+                else
+                    sum.Add(nums[i], i);
+            }
+
+            //if no such match found then return a empty integer array
+            return new int[] { };
+        }
+    }
+}
---
+++ AviPreperation/LeetCodePrep/1.Easy/2.ReverseInteger.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+//Given a 32-bit signed integer, reverse digits of an integer.
+
+//Example 1:
+
+//Input: 123
+//Output: 321
+//Example 2:
+
+//Input: -123
+//Output: -321
+//Example 3:
+
+//Input: 120
+//Output: 21
+//Note:
+//Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1].
+//For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+
+/********My Solution********* Incomplete but Wrong *****/
+namespace AviPreperation.LeetCodePrep._1.Easy
+{
+    public class ReverseSol
+    {
+        public int Reverse(int x)
+        {
+            int val = 0;
+            int mul = 1;
+            var reverseNum = new List<int>();
+
+            while (x > 0)
+            {
+                reverseNum.Add(x % 10);
+                x = x / 10;
+            }
+
+            reverseNum.Reverse();
+            for (int i = 0; i < reverseNum.Count; i++)
+            {
+                val += reverseNum[i] * mul;
+                mul = mul * 10;
+            }
+            return val;
+        }
+
+
+    /********Recommended Solution**************/
+        public int Reverse1(int x)
+        {
+
+            if (x == 0)
+                return 0;
+            bool isGivenIntPositive = x > 0;
+            long result = 0;
+            while (x > 0)
+            {
+                result = result * 10 + x % 10;
+                x = x / 10;
+
+                if (result > int.MaxValue) return 0;
+            }
+
+            return (int)(isGivenIntPositive ? result : -result);
+        }
+    }
+}
