d253099e2a73cc6260945839b6570cb379e23ef0
SaiAvishkarSreerama <SaiAvishkar.Sreerama@gmail.com>
2019-11-06 06:05:23 -0600

    AviCommit: Implementing Stacks Using queues And Queue using Stacks

---
+++ AviPreperation/Data Structures/Queues/ImplementQueueUsingStacks.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AviPreperation.Data_Structures.Queues
+{
+    /* Push()
+     *      time complexity - O(1)
+     *      Space Complexity - O(n)
+     * Pop()
+     *      time complexity - O(1) - amortized, O(n) - worst case
+     *      space compexity - O(1)
+     * Peek(), Empty()
+     *      time complexity - O(1
+     *      space complexity - O(1)
+     */
+    class MyQueue
+    {
+        Stack<int> s1 = new Stack<int>();
+        Stack<int> s2 = new Stack<int>();
+        int front;
+
+        //NOTE: THE COMMENTED CODE FOR EACH METHOD IS APPROACH1, USING 2STACKS, DURING PUSH() ONLY WE ARE TRANSFERING
+        //DATA FROM S1==>S2==>PUSH==>S2==>S1
+
+        //NOTE: THE UNCONMMENTED CODE IS APPROACH2, DIRECTLY PUSHING TO S1
+        //IN POP() S1==>S2==>S2.POP()
+        /** Initialize your data structure here. */
+        public MyQueue()
+        {
+
+        }
+
+        /** Push element x to the back of queue. */
+        public void Push(int x)
+        {
+            //if(Empty())
+            //    front = x;
+            // while(s1.Count > 0)
+            //     s2.Push(s1.Pop());
+            // s1.Push(x);
+            // while(s2.Count > 0)
+            //    s1.Push(s2.Pop());
+
+            if (s1.Count == 0)
+                front = x;
+            s1.Push(x);
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int Pop()
+        {
+            //if(!Empty()){
+            //    front = s1.Peek();
+            //    return s1.Pop();
+            //}
+            //return int.MinValue;
+            if (s2.Count == 0)
+                while (s1.Count > 0)
+                    s2.Push(s1.Pop());
+            return s2.Pop();
+        }
+
+        /** Get the front element. */
+        public int Peek()
+        {
+            //return front;  
+            if (s2.Count > 0)
+                return s2.Peek();
+            return front;
+        }
+
+        /** Returns whether the queue is empty. */
+        public bool Empty()
+        {
+            //return s1.Count == 0;
+
+            return s1.Count == 0 && s2.Count == 0;
+        }
+    }
+}
---
+++ AviPreperation/Data Structures/Stacks/ImplementSatckUsingQueues.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AviPreperation.Data_Structures.Stacks
+{
+    /* Push()
+     *      time complexity - O(n)
+     *      Space Complexity - O(1)
+     * Pop()
+     *      time complexity - O(1) 
+     *      space compexity - O(1)
+     * Peek(), Empty()
+     *      time complexity - O(1
+     *      space complexity - O(1)
+     */
+    class MyStack
+    {
+        Queue<int> q = new Queue<int>();
+        /** Initialize your data structure here. */
+        public MyStack()
+        {
+
+        }
+
+        /** Push element x onto stack. */
+        public void Push(int x)
+        {
+            q.Enqueue(x);
+            int size = q.Count;
+            while (size > 1)
+            {
+                q.Enqueue(q.Dequeue());
+                size--;
+            }
+        }
+
+        /** Removes the element on top of the stack and returns that element. */
+        public int Pop()
+        {
+            return q.Dequeue();
+        }
+
+        /** Get the top element. */
+        public int Top()
+        {
+            return q.Peek();
+        }
+
+        /** Returns whether the stack is empty. */
+        public bool Empty()
+        {
+            return q.Count == 0;
+        }
+    }
+}
