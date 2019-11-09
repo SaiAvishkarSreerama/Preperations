8833616f42ca63db7468d47874141bcbd882ea40
saiAvishkar18 <saiavishkar.sreerama@cerner.com>
2019-11-08 20:12:54 -0600

    AviCommit: 01Matrix-Queue and Keya and Rooms-Stacks

---
+++ AviPreperation/Data Structures/Queues/01Matrix.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AviPreperation.Data_Structures.Queues
+{
+    class _01Matrix
+    {
+        public static int[][] UpdateMatrix(int[][] matrix)
+        {
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+
+            if (m == 0)
+                return matrix;
+
+            Queue<int[]> q = new Queue<int[]>();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i][j] == 0)
+                        q.Enqueue(new int[] { i, j });
+                    else
+                        matrix[i][j] = int.MaxValue;
+                }
+            }
+
+            int[][] dir = new int[4][];
+            dir[0] = new int[] { -1, 0};
+            dir[1] = new int[] { 1, 0};
+            dir[2] = new int[] { 0, -1};
+            dir[3] = new int[] { 0, 1};
+
+            while(q.Count > 0)
+            {
+                int[] cell = q.Dequeue();
+                foreach (int[] d in dir)
+                {
+                    int r = cell[0] + d[0];
+                    int c = cell[1] + d[1];
+
+                    if (r < 0 || r >= m || c < 0 || c >= n || matrix[r][c] <= matrix[cell[0]][cell[1]] + 1)
+                        continue;
+                    q.Enqueue(new int[] {r,c});
+                    matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
+                }
+            }
+
+            return matrix;
+        }
+
+        //static void Main()
+        //{
+        //    int[][] grid = new int[3][];
+        //    //    //Ex-1
+        //    grid[0] = new int[] { 0, 0, 0};
+        //    grid[1] = new int[] { 0, 1, 0};
+        //    grid[2] = new int[] { 0, 0, 0};
+
+        //    UpdateMatrix(grid);
+
+        //}
+    }
+}
---
+++ AviPreperation/Data Structures/Stacks/KeysAndRooms.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AviPreperation.Data_Structures.Stacks
+{
+    public class KeysRoomsClass
+    {
+        //This is a DFS, you visit one room(node) and then the accessible rooms(nodes) with the room keys
+        //if already visited then avoid it(not pushing into stack)
+        public static bool CanVisitAllRooms(IList<IList<int>> rooms)
+        {
+            // stack for rooms to visit
+            // list for visited rooms
+            bool[] seen = new bool[rooms.Count];
+            seen[0] = true;//mark it as visited-true
+
+            Stack<int> s = new Stack<int>(); //0 room is always open, so push it into stack-then we can access the key in room 0
+            s.Push(0);
+
+            while (s.Count > 0)
+            {
+                int keys = s.Pop();//take the top room number
+                foreach (int key in rooms[keys])
+                {//in that room we can expect list of keys, so foreach teh keys
+                    if (!seen[key])
+                    {//if the key number(key for that number room) is not visisted before
+                        seen[key] = true;//visit now 
+                        s.Push(key);//push that key-room number to stack for getting inner keys
+                    }
+                }
+            }
+
+            //finally, loop the seen all rooms and if any room not visited then return false, else true
+            for (int i = 0; i < seen.Length; i++)
+            {
+                if (!seen[i])
+                {
+                    Console.WriteLine("room number " + i + " was not visited");
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main()
+        {
+            List<IList<int>> rooms = new List<IList<int>>()
+            { new List<int> { 2, 3 }
+                ,new List<int> { 2 }
+                ,new List<int> { 3 }
+                ,new List<int> {  } 
+            };
+
+            Console.WriteLine(CanVisitAllRooms(rooms));
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
