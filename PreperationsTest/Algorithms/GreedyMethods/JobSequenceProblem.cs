afd2924a1994c053a81bd8648fca2b8fdeca79f3
Sai Avishkar Sreerama (from Dev Box) <ssreerama@microsoft.com>
2025-03-27 15:18:22 -0500

    DS: Disjoint sets implementation

---
+++ PreperationsTest/Algorithms/GreedyMethods/JobSequenceProblem.cs
@@ -0,0 +1,171 @@
+﻿/*
+ * Problem: Given three arrays id[], deadline[], profit[], where each job i is associated with id[i], deadline[i], and profit[i].
+ * Each job takes 1 unit of time to complete, and only one job can be scheduled at a time. You will earn the profit associated with a job only if it is completed by its deadline.
+ * 
+ * Approach:Greedy method
+ *      1. Form a tuple or a list combining jobId, profit and deadlines
+ *      2. Sort the tuple by profits
+ *      3. Find the maximum deadline so that we know total hours available for to accommodate the jobs
+ *      4. Iterate from last, check if the job can finish before the deadline. Let say 40profit jobId-4 can be done in deadline-1hr.
+ *      5. When any job with deadline-1 comes, we skip it as it is already occupied.
+ *      6. run through all jobs and result will be calculated as the max profits of the assigned jobs
+ * 
+ * Approach 1: Naive Greedy, uses 2 for loops
+ * Approach 2: Priority Queue, single for loop
+ * Approach 3: Using Disjoint sets
+ */
+
+namespace PreperationsTest.Algorithms.GreedyMethods
+{
+    [TestClass]
+    public class JobSequenceProblem
+    {
+        [TestMethod]
+        public void Run()
+        {
+            //// Example 1
+            //int[] jobId = { 1, 2, 3, 4 };
+            //int[] deadline = { 4, 1, 1, 1 };
+            //int[] profit = { 20, 10, 40, 30 };
+            //// Otuput - 40 + 20 = 60
+
+            // Example 2
+            int[] jobId = { 1, 2, 3, 4, 5 };
+            int[] deadline = { 2, 1, 2, 1, 1 };
+            int[] profit = { 100, 19, 27, 25, 15 };
+            // Otuput - 100 + 27 = 127
+
+            JobSequence_NaiveGreedy(jobId, profit, deadline);
+            JobSequence_PriorityQueue(jobId, profit, deadline);
+            JobSequence_DisjointSets(jobId, profit, deadline);
+        }
+
+        /// <summary>
+        /// Approach 1
+        /// Time Complexity: O(N^2)
+        ///      Sorting the list takes 'n logn' time
+        ///      Iterating the list i takes n time, j takes n-1 items - n^2
+        /// space Complexity: O(N)
+        ///      creating a new list/tuple by combining all inputs - O(N)
+        ///      created an array to maintian the scehduled jobs - O(d), where d is the max deadline
+        ///      created few constants - O(1)
+        /// </summary>
+        /// <param name="jobId"></param>
+        /// <param name="profit"></param>
+        /// <param name="deadline"></param>
+        /// <returns></returns>
+        public int JobSequence_NaiveGreedy(int[] jobId, int[] profit, int[] deadline)
+        {
+            int maxDeadline = 0;
+            int maxProfit = 0;
+            int n = jobId.Length;
+
+            // form a tuple with all info
+            List<(int, int, int)> jobInfo = new List<(int, int, int)>();
+            for (int i = 0; i < n; i++)
+            {
+                jobInfo.Add(new(jobId[i], profit[i], deadline[i]));
+                if (deadline[i] > maxDeadline)
+                {
+                    maxDeadline = deadline[i];
+                }
+            }
+
+            // Sort the job info list based on profits, descending b->a
+            jobInfo.Sort((a, b) => b.Item2.CompareTo(a.Item2));
+
+            // Initialize an array to note the assigned jobs
+            bool[] acceptedJobs = new bool[maxDeadline];
+            for (int i = 0; i < n; i++)
+            {
+                // math.min ensures that j starts from the latest possible slot before the job's deadline
+                // In example 2, first high profit job takes the index-1, next high profit job also looks for index-1, but since it is occupied we need to check for next available slot
+                // If the deadline spot is taken, looking for lower index slots can be assigned for the similar deadline job
+                // -1 : gives the array index for length - 4, 0 - 3 indexes
+                for (int j = Math.Min(maxDeadline, jobInfo[i].Item3) - 1; j >= 0; j--)
+                {
+                    // check if the acceptedjob bool array has false means that hour is empty and we can assign a job
+                    if (!acceptedJobs[j])
+                    {
+                        maxProfit += jobInfo[i].Item2;
+                        acceptedJobs[j] = true;
+                        break;
+                    }
+                }
+            }
+            return maxProfit;
+        }
+
+        /// <summary>
+        /// Approach 2: Using Priority Queue:  https://www.geeksforgeeks.org/job-sequencing-problem/#naive-approach-using-greedy-approach-on-2-time-and-on-space
+        /// Time Complexity: O(N LogN)
+        ///      Sorting the list takes 'n logn' time
+        ///      Iterating the list takes N time
+        /// space Complexity: O(N)
+        ///      creating a new list/tuple by combining all inputs - O(N)
+        ///      created an priority queueu - O(N)
+        ///      created few constants - O(1)
+        /// </summary>
+        /// <param name="jobId"></param>
+        /// <param name="profit"></param>
+        /// <param name="deadline"></param>
+        /// <returns></returns>
+        public int JobSequence_PriorityQueue(int[] jobId, int[] profit, int[] deadline)
+        {
+            int maxProfit = 0;
+            int n = jobId.Length;
+
+            // form a tuple with all info
+            List<(int, int, int)> jobInfo = new List<(int, int, int)>();
+            for (int i = 0; i < n; i++)
+            {
+                jobInfo.Add(new(jobId[i], profit[i], deadline[i]));
+            }
+
+            // Sort the job info list based on **deadlines**
+            jobInfo.Sort((a, b) => a.Item3.CompareTo(b.Item3));
+
+            // Initialize an array to note the assigned jobs
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+            foreach (var job in jobInfo)
+            {
+                // Since we are iterating deadlines in ascending order 1,1,1,2,2
+                // first we get deadline-1, since pq is empty, enqueue: pq has 1 item, (profit: 19)
+                // second item, we check deadline-1 and pq.count-1, so go to else and peek-19 < currentProfit-25, dequeu the previous one and enqueue(25)
+                // next time when we get deadline-2 and pq.count-1, enqueu it (100), now pq-count becomes 2
+                // next we get deadline-2 again, goes to else and peek(25, as it FIFO), dequeue and enqueue(27)
+                if (job.Item3 > pq.Count)
+                {
+                    pq.Enqueue(job.Item2, job.Item2);
+                }
+                else if(pq.Count > 0 && pq.Peek() < job.Item2)
+                {
+                    pq.Dequeue();
+                    pq.Enqueue(job.Item2, job.Item2);
+                }
+            }
+
+            //Iterate through the pq and Deququq one by one and add it to maxProfit
+            while(pq.Count > 0)
+            {
+                maxProfit += pq.Dequeue();
+            }
+
+            return maxProfit;
+        }
+
+
+        /// <summary>
+        /// Using Disjoint Sets
+        /// https://www.geeksforgeeks.org/job-sequencing-problem-using-disjoint-set/
+        /// </summary>
+        /// <param name="job"></param>
+        /// <param name="profit"></param>
+        /// <param name="deadline"></param>
+        /// <returns></returns>
+        public int JobSequence_DisjointSets(int[] job, int[] profit, int[] deadline)
+        {
+            return 0;
+        }
+    }
+}
---
+++ PreperationsTest/DataStructures/DisjointSet/DisjointSetImpl.cs
@@ -0,0 +1,137 @@
+﻿/*
+ * Either we can use Rank approach or Size approach.
+ *      if Rank, merging sets will increment the rank
+ *      if Size, merging will adds both sets sizes
+ * Disjoint sets can be implemented using
+ *      1. Arrays
+ *      2. Trees
+ * Time Complexity - O(α(n)), where α(n) is the inverse Ackermann function. This function grows very slowly, making the Find operation nearly constant time in practice. - nearly O(1)
+ * Space Complexity - O(n), we use parent, rank/size arrays
+ */
+
+namespace PreperationsTest.DataStructures.DisjointSet
+{
+    [TestClass]
+    public class DisjointSetImpl
+    {
+        public class DisjointSets
+        {
+            public int[] rank { get; set; }
+            public int[] parent { get; set; }
+            public int[] size { get; set; }
+
+            // Constructor
+            public DisjointSets(int n)
+            {
+                rank = new int[n];
+                parent = new int[n];
+                size = new int[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    parent[i] = i;
+                    rank[i] = 0;
+                    size[i] = 1; //if using size, intialize with 1
+                }
+            }
+
+            public int Find(int x)
+            {
+                if (parent[x] != x)
+                {
+                    parent[x] = Find(parent[x]); // path compression
+                }
+                return parent[x];
+            }
+
+            /// <summary>
+            /// Using Rank
+            /// </summary>
+            /// <param name="x"></param>
+            /// <param name="y"></param>
+            public void Union(int x, int y)
+            {
+                int xRoot = Find(x);
+                int yRoot = Find(y);
+
+                // if both ele has same root means they are in same set
+                if (xRoot == yRoot)
+                {
+                    return;
+                }
+
+                if (rank[xRoot] < rank[yRoot])
+                {
+                    parent[xRoot] = yRoot;
+                }
+                else if (rank[xRoot] > rank[yRoot])
+                {
+                    parent[yRoot] = xRoot;
+                }
+                else
+                {
+                    parent[xRoot] = yRoot;
+                    rank[yRoot]++;
+                }
+            }
+
+            /// <summary>
+            /// Using Size
+            /// </summary>
+            /// <param name="x"></param>
+            /// <param name="y"></param>
+            public void Union_Size(int x, int y)
+            {
+                int xRoot = Find(x);
+                int yRoot = Find(y);
+
+                // if both ele has same root means they are in same set
+                if (xRoot == yRoot)
+                {
+                    return;
+                }
+
+                if (rank[xRoot] <= rank[yRoot])
+                {
+                    parent[xRoot] = yRoot;
+                    size[yRoot] += size[xRoot]; // Since y becoming root, add x size to y, coz now y->y+x
+                }
+                else
+                {
+                    parent[yRoot] = xRoot;
+                    size[xRoot] += size[yRoot]; // Since x becoming root, add y size to x, coz now x->x+y
+                }
+            }
+        }
+
+
+        [TestMethod]
+        public void DisjointSet_Implementation()
+        {
+            // Let there be 5 persons with ids as
+            // 0, 1, 2, 3 and 4
+            int n = 5;
+
+            DisjointSets ds = new DisjointSets(n);
+
+            // 1 is friend of 2
+            ds.Union(1, 2);
+
+            // using size
+            ds.Union_Size(0, 4);
+
+            // 4 is friend of 3
+            ds.Union(4, 3);
+
+            // Find the relationship of 4 and 2
+            if (ds.Find(4) == ds.Find(2))
+            {
+                Console.WriteLine("both are freinds, as they both have same root");
+            }
+            else
+            {
+                Console.WriteLine("4 and 2 belongs to two differnet sets");
+            }
+        }
+    }
+}
