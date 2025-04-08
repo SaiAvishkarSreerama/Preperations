84279d7e6a58df8ae66338c7aae0584d8a6a5d1d
Sai Avishkar Sreerama (from Dev Box) <ssreerama@microsoft.com>
2025-04-08 17:31:47 -0500

    LC: Day1 - 2problems Easy+Mid

---
+++ PreperationsTest/LeetCode/Easy/Array/1_TwoSum.cs
@@ -0,0 +1,69 @@
+﻿/*
+* TimeComplexity - O(N)
+* SpaceComplexity - O(N)
+* 
+* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+* You may assume that each input would have exactly one solution, and you may not use the same element twice.
+* You can return the answer in any order.
+* 
+* Example 1:
+* 
+* Input: nums = [2,7,11,15], target = 9
+* Output: [0,1]
+* Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+* 
+* Example 2:
+* 
+* Input: nums = [3,2,4], target = 6
+* Output: [1,2]
+* 
+* Example 3:
+* 
+* Input: nums = [3,3], target = 6
+* Output: [0,1]
+*/
+namespace PreperationsTest.LeetCode.Easy.Array
+{
+    [TestClass]
+    public class _1_TwoSum
+    {
+        [TestMethod]
+        public void Run()
+        {
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            TwoSum(nums, 9);
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            if (nums.Length == 0)
+                return new int[] { };
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            // Iterate the input array 
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                // To avoid duplicate number consideration, skip when we see the same num in the dict
+                // But here we can use duplicate num, as in quest mentioned we cannot use the same num, but 
+                // simiilar num comes we can use it
+                // if(dict.ContainsKey(nums[i])){
+                //     continue;
+                // }
+
+                // check for the result value in dict
+                int secondNum = target - nums[i];
+                if (dict.ContainsKey(secondNum))
+                {
+                    // if found here, we can return current index and the index in dict
+                    return new int[] { dict[secondNum], i };
+                }
+
+                // add the num as key and index as value in tothe dictionary
+                dict[nums[i]] = i;
+            }
+
+            return new int[] { };
+        }
+    }
+}
---
+++ PreperationsTest/LeetCode/Medium/LinkedLists/2_AddTwoNumbers.cs
@@ -0,0 +1,150 @@
+﻿/*
+* TC - O(N) => O(max(l1 ,l2)) 
+* SC - O(N), 
+* 
+* You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. 
+* Add the two numbers and return the sum as a linked list.
+* You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+* 
+* Example 1:
+* Input: l1 = [2,4,3], l2 = [5,6,4]
+* Output: [7,0,8]
+* Explanation: 342 + 465 = 807.
+* 
+* Example 2:
+* Input: l1 = [0], l2 = [0]
+* Output: [0]
+* 
+* Example 3: 
+* Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+* Output: [8,9,9,9,0,0,0,1]
+*/
+
+namespace PreperationsTest.LeetCode.Medium.LinkedLists
+{
+    [TestClass]
+    public class _2_AddTwoNumbers
+    {
+        [TestMethod]
+        public void Run()
+        {
+            ListNode node3 = new ListNode(3);
+            ListNode node2 = new ListNode(4, node3);
+            ListNode l1 = new ListNode(2, node2);
+
+            ListNode node4 = new ListNode(4);
+            ListNode node6 = new ListNode(6, node4);
+            ListNode l2 = new ListNode(5, node6);
+
+            ListNode result =  AddTwoNumbers(l1, l2);
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            // Listnode to return
+            ListNode resultHead = new ListNode(0);
+
+            // result node to add the iteration sum
+            ListNode result = resultHead;
+
+            // to carry the ieration sum carry
+            int carry = 0;
+            ListNode currL1 = l1;
+            ListNode currL2 = l2;
+
+            // Iterate while l1 or l2 or carry are not empty
+            while (currL1 != null || currL2 != null || carry != 0)
+            {
+                int l1_Val = 0;
+                int l2_Val = 0;
+                if (currL1 != null)
+                {
+                    l1_Val = currL1.val;
+                    currL1 = currL1.next;
+                }
+
+                if (currL2 != null)
+                {
+                    l2_Val = currL2.val;
+                    currL2 = currL2.next;
+                }
+
+                int sum = l1_Val + l2_Val + carry;
+                carry = 0;
+
+                // sum > 9 mean it has two digits, 'carry(Tens) sum(ones)'
+                if (sum > 9)
+                {
+                    carry = (sum / 10) % 10;
+                    sum = sum % 10;
+                }
+
+                // Create new node for the sum and add it to the result, and move result to next
+                ListNode currResult = new ListNode(sum);
+                result.next = currResult;
+                result = result.next;
+            }
+
+            return resultHead.next;
+        }
+
+        /// <summary>
+        /// which utilises the input listnodes without creating any variables. 
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public ListNode AddTwoNumbers_SolFromLeetCode(ListNode l1, ListNode l2)
+        {
+            ListNode r = l1;
+
+            while (l2 != null)
+            {
+
+                l1.val += l2.val;
+
+
+                //if overflows
+                if (l1.val > 9)
+                {
+                    if (l1.next == null)
+                        l1.next = new ListNode();
+
+                    l1.next.val += 1;
+                    l1.val = l1.val % 10;
+                }
+
+                //next
+                if (l1.next == null && l2.next != null)
+                {
+                    l1.next = new ListNode();
+                }
+                if (l1.next != null)
+                    l1 = l1.next;
+                l2 = l2.next;
+            }
+            while (l1.val > 9)
+            {
+                if (l1.next == null)
+                    l1.next = new ListNode();
+
+                l1.next.val += 1;
+                l1.val = l1.val % 10;
+                l1 = l1.next;
+            }
+
+
+            return r;
+        }
+    }
+}
