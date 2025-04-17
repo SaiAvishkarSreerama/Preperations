de1b49e43269f299ca0145c98eb08550a9cda60c
Sai Avishkar Sreerama (from Dev Box) <ssreerama@microsoft.com>
2025-04-17 07:32:22 -0500

    updates

--- PreperationsTest/LeetCode/Easy/Strings/_14_LongestCommonPrefix.cs
+++ PreperationsTest/LeetCode/Easy/Strings/_14_LongestCommonPrefix.cs
@@ -1,8 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ * 
+ * Example 1:
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ * 
+ * Example 2:
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ */
 
 namespace PreperationsTest.LeetCode.Easy.Strings
 {
