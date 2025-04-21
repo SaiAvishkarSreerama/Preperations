43aec86724c2089a3d39f62c17b7bfaa37e04d09
Sai Avishkar Sreerama (from Dev Box) <ssreerama@microsoft.com>
2025-04-21 07:09:24 -0500

    LC: 22.GenerateParenthesis

---
+++ PreperationsTest/LeetCode/Easy/LinkedLists/_21_MergeTwoSortedLists.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+ * Return the head of the merged linked list.
+ * 
+ * Example 1:
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * 
+ * Example 2:
+ * Input: list1 = [], list2 = []
+ * Output: []
+ * 
+ * Example 3:
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ * 
+ * Constraints:
+ * The number of nodes in both lists is in the range [0, 50].
+ * -100 <= Node.val <= 100
+ * Both list1 and list2 are sorted in non-decreasing order.
+ */
+
+namespace PreperationsTest.LeetCode.Easy.LinkedLists
+{
+    [TestClass]
+    public class _21_MergeTwoSortedLists
+    {
+        [TestMethod]
+        public void Runt()
+        {
+
+            ListNode node4 = new ListNode(4);
+            ListNode node2 = new ListNode(2, node4);
+            ListNode node1 = new ListNode(1, node2);
+
+            ListNode node5 = new ListNode(5);
+            ListNode node3 = new ListNode(3, node5);
+            ListNode node11 = new ListNode(1, node3);
+
+            ListNode result = MergeTwoLists(node1, node11);
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        /// <summary>
+        /// Adding two sorted lists
+        /// TC - O(M+N), where m and n are the total nodes in lists1 and list2
+        /// SC - O(M+N)
+        /// </summary>
+        /// <param name="list1"></param>
+        /// <param name="list2"></param>
+        /// <returns></returns>
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode result = new ListNode();
+            ListNode resultHead = result;
+
+            while (list1 != null && list2 != null)
+            {
+                ListNode curr = new ListNode();
+                if (list1.val <= list2.val)
+                {
+                    curr.val = list1.val;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    curr.val = list2.val;
+                    list2 = list2.next;
+                }
+                result.next = curr;
+                result = result.next;
+            }
+
+            // append list1 remaining nodes
+            if (list1 != null)
+            {
+                result.next = list1;
+            }
+
+            // append list2 remaining nodes
+            if (list2 != null)
+            {
+                result.next = list2;
+            }
+
+            return resultHead.next;
+        }
+    }
+}
---
+++ PreperationsTest/LeetCode/Medium/BackTracking/_22_GenerateParanthesis.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * 
+ * Example 1:
+ * Input: n = 3
+ * Output: ["((()))","(()())","(())()","()(())","()()()"]
+ * 
+ * Example 2:
+ * Input: n = 1
+ * Output: ["()"]
+ * 
+ * Constraints:
+ * 1 <= n <= 8
+ *  */
+using System.Threading.Channels;
+
+namespace PreperationsTest.LeetCode.Medium.BackTracking
+{
+    [TestClass]
+    public class _22_GenerateParanthesis
+    {
+        [TestMethod]
+        public void Run()
+        {
+            GenerateParenthesis(3);
+        }
+
+        private IList<string> result { get; set; }
+        private int totalPairs { get; set; }
+
+
+        /// <summary>
+        /// We are generating all possible valid combinations of the parenthesis for a given count
+        /// if n = 6 means, for total we have six pairs of parantheses '()', 6-open and 6-closed paranthesis
+        /// Using Backtracking
+        /// TC - O(4^n/ sq.root(n)), using Catalan number 
+        /// SC - O(Cn) where Cn is the n-th Catalan number.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public IList<string> GenerateParenthesis(int n)
+        {
+            totalPairs = n;
+            result = new List<string>();
+            BackTracking(0, 0, "");
+
+            return result;
+        }
+
+        public void BackTracking(int open, int close, string curr)
+        {
+            // Base case - when open-close parenthesis are equal and we reach parenthesis limit
+            if (open == totalPairs && close == totalPairs)
+            {
+                result.Add(curr);
+                return;
+            }
+
+            // when to add open parenthesis: when we have limit
+            if (open < totalPairs)
+            {
+                BackTracking(open + 1, close, curr + "(");
+            }
+
+            // when to add closed parenthesis: when open is higher
+            if (close < open)
+            {
+                BackTracking(open, close + 1, curr + ")");
+
+            }
+
+        }
+    }
+}
