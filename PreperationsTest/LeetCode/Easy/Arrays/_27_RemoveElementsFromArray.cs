c80618aa4676c6e4f3e5a21de76815e6b06d963b
Sai Avishkar Sreerama (from Dev Box) <ssreerama@microsoft.com>
2025-04-23 08:50:20 -0500

    LC: 27_remove Elements

---
+++ PreperationsTest/LeetCode/Easy/Arrays/_27_RemoveElementsFromArray.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+ * Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+ * Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+ * Return k.
+ * 
+ * Custom Judge:
+ * The judge will test your solution with the following code:
+ * int[] nums = [...]; // Input array
+ * int val = ...; // Value to remove
+ * int[] expectedNums = [...]; // The expected answer with correct length.
+ *                             // It is sorted with no values equaling val.
+ * int k = removeElement(nums, val); // Calls your implementation
+ * assert k == expectedNums.length;
+ * sort(nums, 0, k); // Sort the first k elements of nums
+ * for (int i = 0; i < actualLength; i++) {
+ *     assert nums[i] == expectedNums[i];
+ * }
+ * If all assertions pass, then your solution will be accepted.
+ * 
+ * Example 1:
+ * Input: nums = [3,2,2,3], val = 3
+ * Output: 2, nums = [2,2,_,_]
+ * Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ * 
+ * Example 2:
+ * Input: nums = [0,1,2,2,3,0,4,2], val = 2
+ * Output: 5, nums = [0,1,4,0,3,_,_,_]
+ * Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+ * Note that the five elements can be returned in any order.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ * 
+ * Constraints:
+ * 0 <= nums.length <= 100
+ * 0 <= nums[i] <= 50
+ * 0 <= val <= 100
+ *  
+ *  TC - O(N), traversing all numbers once with two pointer
+ *  SC - O(1), Inplace updation 
+ */
+
+namespace PreperationsTest.LeetCode.Easy.Arrays
+{
+    [TestClass]
+    public class _27_RemoveElementsFromArray
+    {
+        [TestMethod]
+        public void Run()
+        {
+            int[] arr = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int k = RemoveElement(arr, 2);
+        }
+
+        /// <summary>
+        /// Like remove duplicate, we do two pointer technique (there we compare i and j values and replace ++i with j, here j!=val, replace i++ with j)
+        /// fixing i and moving j, when j != val, replace i with j and move next
+        /// This way we dont leave any non-val left behind
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public int RemoveElement(int[] nums, int val)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int i = 0;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] != val)
+                {
+                    nums[i++] = nums[j];
+                }
+            }
+
+            return i;
+        }
+    }
+}
