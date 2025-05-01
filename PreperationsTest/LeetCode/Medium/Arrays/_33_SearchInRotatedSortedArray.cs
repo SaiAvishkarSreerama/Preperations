b6a56a4ac103fd16d8796c8c56c48e9b67048c6a
Sai Avishkar Sreerama (from Dev Box) <ssreerama@microsoft.com>
2025-05-01 07:51:10 -0500

    LC: 33 & 34

---
+++ PreperationsTest/LeetCode/Medium/Arrays/_33_SearchInRotatedSortedArray.cs
@@ -0,0 +1,98 @@
+﻿/*
+ * There is an integer array nums sorted in ascending order (with distinct values).
+ * Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+ * Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 
+ * Example 1:
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ * 
+ * Example 2:
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ * 
+ * Example 3:
+ * Input: nums = [1], target = 0
+ * Output: -1
+ * 
+ * Constraints:
+ * 1 <= nums.length <= 5000
+ * -104 <= nums[i] <= 104
+ * All values of nums are unique.
+ * nums is an ascending array that is possibly rotated.
+ * -104 <= target <= 104
+ */
+
+namespace PreperationsTest.LeetCode.Medium.Arrays
+{
+    [TestClass]
+    public class _33_SearchInRotatedSortedArray
+    {
+        [TestMethod]
+        public void Run()
+        {
+            int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+            Search(nums, 0);
+        }
+
+        /// <summary>
+        /// Binary Search
+        /// TC - O (LogN)
+        /// SC - O(1)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int Search(int[] nums, int target)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return -1;
+            }
+
+            int low = 0;
+            int high = nums.Length - 1;// (low + high) /2
+
+            while (low <= high)
+            {
+                // Base case
+                int mid = low - (low - high) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+
+                // Check if the left side is sorted
+                if (nums[low] <= nums[mid])
+                {
+                    //check if the target is in left
+                    if (nums[low] <= target && target < nums[mid])
+                    {
+                        high = mid - 1;
+                    }
+                    else
+                    {
+                        // even though left is sorted, but our target is not there, so move to right part
+                        low = mid + 1;
+                    }
+                }
+                // else right is sorted
+                if (nums[mid] <= nums[high])
+                {
+                    //check if the target is in left
+                    if (nums[mid] < target && target <= nums[high])
+                    {
+                        low = mid + 1;
+                    }
+                    else
+                    {
+                        // even though right is sorted, but our target is not there, so move to left part
+                        high = mid - 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ PreperationsTest/LeetCode/Medium/Arrays/_34_FindFirstAndLastPositionOfElementInSortedArray.cs
@@ -0,0 +1,85 @@
+﻿/* Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+ * If target is not found in the array, return [-1, -1].
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 
+ * Example 1:
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ * 
+ * Example 2:
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ * 
+ * Example 3:
+ * Input: nums = [], target = 0
+ * Output: [-1,-1]
+*/
+
+namespace PreperationsTest.LeetCode.Medium.Arrays
+{
+    [TestClass]
+    public class _34_FindFirstAndLastPositionOfElementInSortedArray
+    {
+        [TestMethod]
+        public void Run()
+        {
+            int[] nums = { 2, 2 };
+            SearchRange(nums, 2);
+        }
+
+        /// <summary>
+        /// Binary Search
+        /// TC - O (LogN)
+        /// SC - O(1)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] SearchRange(int[] nums, int target)
+        {
+            int[] result = new int[] { -1, -1 };
+            if (nums == null || nums.Length == 0)
+            {
+                return result;
+            }
+
+            int low = 0;
+            int high = nums.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+
+                // If mid is target, then check if the same num are repeated in front of it and after
+                if (nums[mid] == target)
+                {
+                    int first = mid, last = mid;
+                    // if our previous num also same as target, move back till we see diff num
+                    while (first > low && nums[first] == target)
+                        first--;
+                    // if our next num also same as target, move next till we see diff num
+                    while (last < high && nums[last] == target)
+                        last++;
+
+                    // Now we have first and last occurances of the target
+                    result[0] = first;
+                    result[1] = last;
+
+                    // break the loop here, otehrwise, low=mid+1 and we loose this result
+                    break;
+                }
+
+                if (nums[mid] > target)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
