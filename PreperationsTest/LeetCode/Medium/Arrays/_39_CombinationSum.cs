8d1bc7ba55dfa3a9e799342230dfe8b8580bff6a
Sai Avishkar Sreerama (from Dev Box) <ssreerama@microsoft.com>
2025-05-13 08:39:58 -0500

    LC: Combination sum I and II

---
+++ PreperationsTest/LeetCode/Medium/Arrays/_39_CombinationSum.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+ * The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+ * The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+ * 
+ * Example 1:
+ * Input: candidates = [2,3,6,7], target = 7
+ * Output: [[2,2,3],[7]]
+ * Explanation:
+ * 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+ * 7 is a candidate, and 7 = 7.
+ * These are the only two combinations.
+ * 
+ * Example 2:
+ * Input: candidates = [2,3,5], target = 8
+ * Output: [[2,2,2,2],[2,3,3],[3,5]]
+ * 
+ * Example 3:
+ * Input: candidates = [2], target = 1
+ * Output: []
+ * 
+ * TC - O(2^T), the algorithm explores all combinations of candidates that sum up to the target. Since each candidate can be chosen multiple times, the recursion tree can grow exponentially
+ *      The branching factor is up to n (number of candidates), and the depth is roughly target / min(candidates).
+ * SC - O(T), The maximum depth of the recursion stack is proportional to the target value (in the worst case, you keep adding the smallest candidate until you reach the target).
+ * */
+namespace PreperationsTest.LeetCode.Medium.Arrays
+{
+    [TestClass]
+    public class _39_CombinationSum
+    {
+        [TestMethod]
+        public void Run()
+        {
+            int[] nums = { 2, 3, 6, 7 };
+            IList<IList<int>> result = CombinationSum(nums, 7);
+        }
+
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            combinationSumRecursion(candidates, target, 0, new List<int>(), result);
+
+            return result;
+        }
+
+        public void combinationSumRecursion(int[] candidates, int target, int currIndex, List<int> currList, List<IList<int>> result)
+        {
+            if (target == 0)
+            {
+                result.Add(new List<int>(currList));
+            }
+
+            for (int i = currIndex; i < candidates.Length; i++)
+            {
+                if (candidates[i] <= target)
+                {
+                    currList.Add(candidates[i]);
+
+                    combinationSumRecursion(candidates, target - candidates[i], i, currList, result);
+
+                    currList.RemoveAt(currList.Count - 1);
+                }
+            }
+        }
+    }
+}
---
+++ PreperationsTest/LeetCode/Medium/Arrays/_40_CombinationSumII.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+ * Each number in candidates may only be used once in the combination.
+ * Note: The solution set must not contain duplicate combinations.
+ * 
+ * Example 1:
+ * Input: candidates = [10,1,2,7,6,1,5], target = 8
+ * Output: 
+ * [[1,1,6],[1,2,5],[1,7],[2,6]]
+ * 
+ * Example 2:
+ * Input: candidates = [2,5,2,1,2], target = 5
+ * Output: 
+ * [[1,2,2],[5]]
+ * 
+ * TC - O(2^n + n log n), nlogn for sorting, each element can either be included or excluded, and duplicates are skipped using sorting and a check. This is tighter than CombinationSum because elements are not reused.
+ * SC - O(n), recursion depth is at most n, and each path can hold up to n elements.
+ * */
+namespace PreperationsTest.LeetCode.Medium.Arrays
+{
+    [TestClass]
+    public class _40_CombinationSumII
+    {
+        [TestMethod]
+        public void Run()
+        {
+            int[] nums = { 2, 3, 6, 7 };
+            IList<IList<int>> result = CombinationSum2(nums, 7);
+        }
+
+        public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            List<int> curr = new List<int>();
+            Array.Sort(candidates);
+            combinationSumRecursion(candidates, target, 0, curr, result);
+
+            return result;
+        }
+
+        public void combinationSumRecursion(int[] candidates, int target, int currIndex, List<int> currList, List<IList<int>> result)
+        {
+            if (target < 0)
+            {
+                return;
+            }
+            else if (target == 0)
+            {
+                result.Add(new List<int>(currList));
+            }
+            else
+            {
+
+                for (int i = currIndex; i < candidates.Length; i++)
+                {
+                    if (i > currIndex && candidates[i] == candidates[i - 1])
+                    {
+                        continue;
+                    }
+                    if (candidates[i] <= target)
+                    {
+                        currList.Add(candidates[i]);
+
+                        combinationSumRecursion(candidates, target - candidates[i], i + 1, currList, result);
+
+                        currList.RemoveAt(currList.Count - 1);
+                    }
+                }
+            }
+        }
+    }
+}
