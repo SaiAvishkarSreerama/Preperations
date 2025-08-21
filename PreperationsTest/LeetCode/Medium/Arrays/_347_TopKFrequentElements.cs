6db565e80b5c7bc6700aafe77626012b77aa7a32
Sai Avishkar Sreerama (from Dev Box) <ssreerama@microsoft.com>
2025-08-21 16:38:41 -0500

    LC: top k

---
+++ PreperationsTest/LeetCode/Medium/Arrays/_347_TopKFrequentElements.cs
@@ -0,0 +1,80 @@
+﻿/*
+*  Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+* Example 1:
+* Input: nums = [1,1,1,2,2,3], k = 2
+* Output: [1,2]
+* 
+* Example 2:
+* Input: nums = [1], k = 1
+* Output: [1]
+* 
+* Constraints:
+* 1 <= nums.length <= 105
+* -104 <= nums[i] <= 104
+* k is in the range [1, the number of unique elements in the array].
+* It is guaranteed that the answer is unique.
+* */
+
+namespace PreperationsTest.LeetCode.Medium.Arrays
+{
+    [TestClass]
+    public class _347_TopKFrequentElements
+    {
+        [TestMethod]
+        public void Run()
+        {
+            int[] nums = [1, 1, 1, 2, 2, 3];
+            int k = 2;
+            TopKFrequent(nums, k);
+        }
+
+        /// <summary>
+        /// TC - O(N logk)
+        /// SC - O(n)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            // edge case
+            if (nums.Length == k)
+            {
+                return nums;
+            }
+            // Algo:
+            // TC - O(n);
+            // step1: count the num and its frequency in a dictionary
+            Dictionary<int, int> frequency = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!frequency.ContainsKey(nums[i]))
+                {
+                    frequency.Add(nums[i], 0);
+                }
+                frequency[nums[i]]++;
+            }
+
+            //step 2: create a Heap/priorityQueue to keep top k elements only
+            // TC - O(N logk)
+            PriorityQueue<int, int> heap = new PriorityQueue<int, int>();
+            foreach (var pair in frequency)
+            {
+                heap.Enqueue(pair.Key, pair.Value);
+                if (heap.Count > k)
+                {
+                    heap.Dequeue();
+                }
+            }
+
+            // step3: collect the k elements from the heap and return the restult
+            int[] result = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                result[i] = heap.Dequeue();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ PreperationsTest/LeetCode/Medium/Arrays/_56_MergeIntervals.cs
@@ -0,0 +1,60 @@
+﻿
+/*
+* Given a collection of intervals, merge all overlapping intervals.
+* 
+* Example 1:
+* 
+* Input: [[1,3],[2,6],[8,10],[15,18]]
+* Output: [[1,6],[8,10],[15,18]]
+* Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+* Example 2:
+* 
+* Input: [[1,4],[4,5]]
+* Output: [[1,5]]
+* Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+* NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature. 
+* */
+
+namespace PreperationsTest.LeetCode.Medium.Arrays
+{
+    [TestClass]
+    public class _56_MergeIntervals
+    {
+        [TestMethod]
+        public void Run()
+        {
+            int[][] input = [[1, 3], [2, 6], [8, 10], [15, 18]];
+            int[][] output = Merge(input);
+        }
+
+        // TC - O(nlogn), sorting
+        // SC - O(n)
+        public int[][] Merge(int[][] intervals)
+        {
+            if (intervals.Length == 0)
+                return new int[0][];
+
+            // Sort the arrays based on their start value
+            Array.Sort(intervals, (a, b) => a[0] - b[0]);
+
+            List<int[]> result = new List<int[]>();
+            result.Add(intervals[0]);
+
+            //Iterate all other intervals and merge with the prev intervals
+            foreach (var curr in intervals)
+            {
+                int[] prev = result.Last();
+                if (prev[1] >= curr[0])
+                {
+                    prev[1] = Math.Max(prev[1], curr[1]);
+                }
+                else
+                {
+                    result.Add(curr);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
